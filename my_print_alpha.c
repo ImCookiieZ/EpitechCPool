be0c8d3a9b362fd2a3349cfe2de2123f3fbb2457
Karl Stoerzel <CookiieZ@localhost.localdomain>
2019-10-02 05:19:17 -0400

    empty

---
+++ my_print_alpha.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** my_aloha
+** File description:
+** 
+*/
+int my_print_alpha(void)
+{
+    int my_char=97;
+
+    while (my_char < 123) { 
+        my_putchar(my_char);
+        my_char++;
+    }
+    return(0);
+}
